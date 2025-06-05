edef023c74f6b8dde7abbd34d918c79d2041e9d1
curio-bytes <studentdukmv@gmail.com>
2025-06-05 13:20:23 +0530

    Update curio-bytes.sh

--- It Speaks! Create Synthetic Speech Using Text-to-Speech/curio-bytes.sh
+++ It Speaks! Create Synthetic Speech Using Text-to-Speech/curio-bytes.sh
@@ -15,3 +15,8 @@ gcloud iam service-accounts create tts-qwiklab
 gcloud iam service-accounts keys create tts-qwiklab.json --iam-account tts-qwiklab@$PROJECT_ID.iam.gserviceaccount.com
 
 export GOOGLE_APPLICATION_CREDENTIALS=tts-qwiklab.json
+
+echo
+echo "${MAGENTA_TEXT}${BOLD_TEXT}LIKE AND SUBSCRIBE MY YOUTUBE CHANNEL${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}-> https://youtube.com/@curio_bytes_15${RESET_FORMAT}"
+echo
