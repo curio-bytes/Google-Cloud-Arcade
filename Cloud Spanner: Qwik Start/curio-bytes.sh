ade21d605c2e54a0770be9f721d956f23551b29d
Curio Bytes <studentdukmv@gmail.com>
2025-06-19 14:54:31 +0530

    Update curio-bytes.sh

--- Cloud Spanner: Qwik Start/curio-bytes.sh
+++ Cloud Spanner: Qwik Start/curio-bytes.sh
@@ -1,3 +1,25 @@
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+
+RESET_FORMAT=$'\033[0m'
+BOLD_TEXT=$'\033[1m'
+UNDERLINE_TEXT=$'\033[4m'
+clear
+
+echo
+echo "${BLUE_TEXT}${BOLD_TEXT}=======================================${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}         Solution from Curio Bytes     ${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}=======================================${RESET_FORMAT}"
+echo
+
+echo "${CYAN_TEXT}${BOLD_TEXT}... Starting Execution ... ${RESET_FORMAT}"
+echo
 
 gcloud auth list
 
@@ -22,3 +44,7 @@ gcloud spanner databases ddl update example-db --instance=test-instance \
     SingerInfo BYTES(MAX),
     BirthDate DATE,
     ) PRIMARY KEY(SingerId);"
+
+echo
+echo "${GREEN_TEXT}${BOLD_TEXT}--------- Congratulations for Completing the lab !! ---------${RESET_FORMAT}"
+echo
