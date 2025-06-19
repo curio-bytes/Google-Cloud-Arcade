5757215ff8c1fb645db5214f90c6efffb1a4af4b
Curio Bytes <studentdukmv@gmail.com>
2025-06-19 13:50:57 +0530

    Update curio-bytes.sh

--- Infrastructure as Code with Terraform/curio-bytes.sh
+++ Infrastructure as Code with Terraform/curio-bytes.sh
@@ -1,3 +1,25 @@
+#!/bin/bash
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+DIM_TEXT=$'\033[2m'
+STRIKETHROUGH_TEXT=$'\033[9m'
+BOLD_TEXT=$'\033[1m'
+RESET_FORMAT=$'\033[0m'
+
+clear
+
+echo
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}     Solution from Curio Bytes     ${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo
+
 
 gcloud auth list
 
@@ -272,3 +294,7 @@ resource "google_compute_instance" "another_instance" {
 
 terraform plan
 terraform apply -auto-approve
+
+echo
+echo "${MAGENTA_TEXT}${BOLD_TEXT}Congratulations for completing the lab !${RESET_FORMAT}"
+echo
