333245ea7e4d8fb799f81740262c049f58b4419e
Curio Bytes <studentdukmv@gmail.com>
2025-06-19 14:21:00 +0530

    Update curio-bytes.sh

--- Set Up Network and Application Load Balancers/curio-bytes.sh
+++ Set Up Network and Application Load Balancers/curio-bytes.sh
@@ -1,3 +1,27 @@
+#!/bin/bash
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+DIM_TEXT=$'\033[2m'
+STRIKETHROUGH_TEXT=$'\033[9m'
+BOLD_TEXT=$'\033[1m'
+RESET_FORMAT=$'\033[0m'
+
+clear
+
+echo
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}     Solution from Curio Bytes     ${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo
+
+echo "${GREEN_TEXT}${BOLD_TEXT}... Starting Execution ...${RESET_FORMAT}"
+echo
 
 gcloud auth list
 
@@ -140,3 +164,9 @@ gcloud compute forwarding-rules create http-content-rule \
    --global \
    --target-http-proxy=http-lb-proxy \
    --ports=80
+
+
+
+echo
+echo "${MAGENTA_TEXT}${BOLD_TEXT}Congratulations for completing the lab !${RESET_FORMAT}"
+echo
