c1415c857fbe46156d86390e60aecfe7b46d1d51
Curio Bytes <studentdukmv@gmail.com>
2025-06-20 13:36:00 +0530

    Create curio-bytes.sh

---
+++ Detect Labels, Faces, and Landmarks in Images with the Cloud Vision API/curio-bytes.sh
@@ -0,0 +1,65 @@
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo
+echo "${YELLOW}${BOLD}===================================${RESET_FORMAT}"
+echo "${YELLOW}${BOLD}     Solution from Curio Bytes     ${RESET_FORMAT}"
+echo "${YELLOW}${BOLD}===================================${RESET_FORMAT}"
+echo
+
+echo "${BG_MAGENTA}${BOLD}... Starting Execution ...${RESET}"
+echo
+
+gcloud auth list
+
+gcloud alpha services api-keys create --display-name="CurioBytes" 
+
+KEY_NAME=$(gcloud alpha services api-keys list --format="value(name)" --filter "displayName=CurioBytes")
+
+export API_KEY=$(gcloud alpha services api-keys get-key-string $KEY_NAME --format="value(keyString)")
+
+export PROJECT_ID=$(gcloud config list --format 'value(core.project)')
+
+gsutil mb gs://$PROJECT_ID
+
+
+curl -L -o donuts.png https://github.com/Techcps/Google-Cloud-Skills-Boost/master/Detect%20Labels%2C%20Faces%2C%20and%20Landmarks%20in%20Images%20with%20the%20Cloud%20Vision%20API/donuts.png
+
+curl -L -o selfie.png https://github.com/Techcps/Google-Cloud-Skills-Boost/master/Detect%20Labels%2C%20Faces%2C%20and%20Landmarks%20in%20Images%20with%20the%20Cloud%20Vision%20API/selfie.png
+
+curl -L -o city.png https://github.com/Techcps/Google-Cloud-Skills-Boost/master/Detect%20Labels%2C%20Faces%2C%20and%20Landmarks%20in%20Images%20with%20the%20Cloud%20Vision%20API/city.png
+
+
+gsutil cp donuts.png gs://$PROJECT_ID/donuts.png
+
+gsutil cp selfie.png gs://$PROJECT_ID/selfie.png
+
+gsutil cp city.png gs://$PROJECT_ID/city.png
+
+gcloud storage objects update gs://$PROJECT_ID/donuts.png --add-acl-grant=entity=AllUsers,role=READER
+
+gcloud storage objects update gs://$PROJECT_ID/selfie.png --add-acl-grant=entity=AllUsers,role=READER
+
+gcloud storage objects update gs://$PROJECT_ID/city.png --add-acl-grant=entity=AllUsers,role=READER
+
+echo
+echo "${CYAN}${BOLD}---------- Congratulations For Completing The Lab !!! ------------${RESET}"
